93ce11e8b3daa7538c962df7bf2b57a80a221b92
David Esquerra <davidesquerra.de@gmail.com>
2022-04-24 15:06:05 -0700

    got gui in a seperate file using module

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 import tkinter as tk
 from plugins.roku import Roku
+from plugins.gui import Gui
 
-my_device = Roku("192.168.1.64", "8060")
 
-window = tk.Tk()
 
-window.title("Roku Remote")
 
-frame = tk.Frame(window) 
+def main():
 
-left = tk.Button(frame, text='left', command=my_device.left)
-right = tk.Button(frame, text='right',command=my_device.right)
-up = tk.Button(frame, text='up', command=my_device.up)
-down = tk.Button(frame, text='down', command=my_device.down)
-home = tk.Button(frame, text='home', command=my_device.roku_home)
-rev = tk.Button(frame, text='reverse', command=my_device.reverse)
-forward = tk.Button(frame, text='forward', command=my_device.fast_forward)
-select = tk.Button(frame, text='select', command=my_device.select)
+    my_device = Roku("192.168.1.64", "8060")
 
-up.grid(row=0, columnspan=2, pady=10, ipady=20, ipadx=20)
-left.grid(row=1)
-right.grid(row=1, column=1)
-down.grid(row=2, columnspan=2, pady=10)
-home.grid(row=4, columnspan=2, pady=10)
-rev.grid(row=3, column=0, pady=15)
-forward.grid(row=3, column=1)
-select.grid(row=5, columnspan=2, pady=10)
+    root = tk.Tk()
+    app = Gui(root, my_device)
+    root.mainloop()
 
-frame.pack(expand=True)
 
-window.mainloop()
+
+
+main()
--- plugins/gui.py
+++ plugins/gui.py
@@ -1,22 +1,39 @@
 import tkinter as tk
 
 
-
-
 class Gui:
-    def __init__(self, window):
+    def __init__(self, window, device):
         self.window = window
-        self.title = self.window.title("h")
+        self.title = self.window.title("Roku Remote")
         self.frame = tk.Frame(self.window)
 
-        self.button = tk.Button(self.frame, text='hello')
-        self.button.pack()
-        self.frame.pack()
+        self.device = device
+
+        self.left = tk.Button(self.frame, text='left', command=self.device.left)
+        self.right = tk.Button(self.frame, text='right',command=self.device.right)
+        self.up = tk.Button(self.frame, text='up', command=self.device.up)
+        self.down = tk.Button(self.frame, text='down', command=self.device.down)
+        self.home = tk.Button(self.frame, text='home', command=self.device.roku_home)
+        self.rev = tk.Button(self.frame, text='reverse', command=self.device.reverse)
+        self.forward = tk.Button(self.frame, text='forward', command=self.device.fast_forward)
+        self.select = tk.Button(self.frame, text='select', command=self.device.select)
+
+        self.up.grid(row=0, columnspan=2, pady=10, ipady=20, ipadx=20)
+        self.left.grid(row=1)
+        self.right.grid(row=1, column=1)
+        self.down.grid(row=2, columnspan=2, pady=10)
+        self.home.grid(row=4, columnspan=2, pady=10)
+        self.rev.grid(row=3, column=0, pady=15)
+        self.forward.grid(row=3, column=1)
+        self.select.grid(row=5, columnspan=2, pady=10)
+
+        self.frame.pack(expand=True)
 
 
 
 
 
-root = tk.Tk()
-app = Gui(root)
-root.mainloop()
+
+#root = tk.Tk()
+#app = Gui(root, my_device)
+#root.mainloop()
