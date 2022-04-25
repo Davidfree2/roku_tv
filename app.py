1488ca7869579b84019248d961f883186832c213
David Esquerra <davidesquerra.de@gmail.com>
2022-04-24 18:31:35 -0700

    added linux executable file that contains all the classes and everything. Th purpose of this is to move a single file into .local/bin folder and make it executable

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import tkinter as tk
 from plugins.roku import Roku
 from plugins.gui import Gui
@@ -7,6 +9,7 @@ from plugins.gui import Gui
 
 def main():
 
+    #change device based on your ip address and port number
     my_device = Roku("192.168.1.64", "8060")
 
     root = tk.Tk()
